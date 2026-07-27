4a7dc26a1a28ab95ae820ddd14f305047e39d5ab
xianzeYao <13812663063yxz@gmail.com>
2026-07-27 20:54:53 +0800

    update robocasa training config

--- examples/modelExtensions/CoT/scripts/run_qwen35_gr00t_robocasa_fourier_CoT_v1.sh
+++ examples/modelExtensions/CoT/scripts/run_qwen35_gr00t_robocasa_fourier_CoT_v1.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 cd /home/yxz/CoT/CoT_vla
 export WANDB_MODE="${WANDB_MODE:-offline}"
 
-exec /root/data/yxz/miniforge3/envs/CoT/bin/accelerate launch \
+exec /root/data/yxz/miniforge3/envs/CoT_linearATT/bin/accelerate launch \
   --config_file starVLA/config/deepseeds/deepspeed_zero2.yaml \
   --num_processes "${NUM_GPUS:-4}" \
-  --main_process_port "${MAIN_PROCESS_PORT:-0}" \
+  --main_process_port "${MAIN_PROCESS_PORT:-20252}" \
   starVLA/training/train_starvla_cot_v1.py \
   --config_yaml examples/modelExtensions/CoT/configs/qwen35_gr00t_robocasa_fourier_CoT_v1.yaml \
   "$@"
